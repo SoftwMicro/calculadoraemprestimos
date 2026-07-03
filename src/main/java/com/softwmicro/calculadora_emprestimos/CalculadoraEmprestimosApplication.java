e70e75db443840cbc159c276c2e2620cfb70233a
Manoel Neto <manoelcontatoneto@gmail.com>
2026-07-03 17:02:41 -0300

    Projeto inicial - Estrutura inicial do projeto

---
+++ src/main/java/com/softwmicro/calculadora_emprestimos/CalculadoraEmprestimosApplication.java
@@ -0,0 +1,13 @@
+package com.softwmicro.calculadora_emprestimos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraEmprestimosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraEmprestimosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwmicro/calculadora_emprestimos/CalculadoraEmprestimosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwmicro.calculadora_emprestimos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraEmprestimosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
